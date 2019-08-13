bcd1ac23d1e19fc13f526511f273dbeb62dc1288
Joonas <lebbis63@gmail.com>
2019-08-13 12:35:36 +0300

    13.8
    Animaatioita tehty

---
+++ Assets/animController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour
+{
+
+    Animator animator;
+    private bool reverse;
+    private bool forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+
+    public void ButtonClick()
+    {
+
+        animator.SetBool("reverse", reverse);
+        animator.SetBool("forward", forward);
+
+        reverse = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
