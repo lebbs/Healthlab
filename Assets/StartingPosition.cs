38187389c2554e18bd2278ab8d7b0cafe5272754
Joonas <lebbis63@gmail.com>
2019-09-19 11:51:39 +0300

    UUsin
    uusin versio

--- Assets/StartingPosition.cs
+++ Assets/StartingPosition.cs
@@ -8,7 +8,7 @@ public class StartingPosition : MonoBehaviour
     Vector3 startPos;
     Vector3 startScale;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         startPos = this.transform.position;
         startRotation = this.transform.rotation;
@@ -18,6 +18,7 @@ public class StartingPosition : MonoBehaviour
 
 
     }
+
 
     public void StarPosition()
     {
